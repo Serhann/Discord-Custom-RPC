5ae5505ede608dda991f41aad676439268401e1f
Serhann <serhanpw@gmail.com>
2017-12-11 01:26:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const { Client } = require('discord-rpc');
-const { app } = require('./config.json');
+const { Client } = require('discord-rpc')
+const { app } = require('./config.json')
 
-const rpc = new Client({ transport: app.transport });
+const rpc = new Client({ transport: app.transport })
 
 rpc.on('ready', () => {
-  console.log('RPC ready!');
+  console.log('RPC ready!')
 
   rpc.setActivity({
     details: app.details,
@@ -13,7 +13,7 @@ rpc.on('ready', () => {
     largeImageText: app.largeImageText,
     smallImageKey: app.smallImageKey,
     smallImageText: app.smallImageText
-  });
-});
+  })
+})
 
-rpc.login(app.id).catch(console.error);
+rpc.login(app.id).catch(console.error)
